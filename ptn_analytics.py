70d8b33762a79d5acae16ea5d4c0c2931f6ca747
sandrofsousa <sandrofsousa@gmail.com>
2014-09-25 15:53:00 -0300

    First commit
    first commit

---
+++ ptn_analytics.py
@@ -0,0 +1,6 @@
+# Public Transport Network of São Paulo as a Complex Network
+#
+# Analysis of GTFS (Bus, subway adn train system) data from SPTrans obtained on
+#
+# This first approach focus on an empirical analysis of the Network using graph theory,
+# calculating degree distribution, centrality, hubs, clusters and another network metrics
