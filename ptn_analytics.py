0b0a7f29e84f5fdce7cef75d9790c2c9bdb298c2
sandrofsousa <sandrofsousa@gmail.com>
2014-09-25 19:11:25 -0300

    not changed

--- ptn_analytics.py
+++ ptn_analytics.py
@@ -1,6 +1,12 @@
-# Public Transport Network of São Paulo as a Complex Network
 #
-# Analysis of GTFS (Bus, subway adn train system) data from SPTrans obtained on
+# ----- Public Transport Network of São Paulo as a Complex Network -----
+#
+# Analysis of GTFS data (Bus, subway adn train system) from SPTrans.
 #
 # This first approach focus on an empirical analysis of the Network using graph theory,
 # calculating degree distribution, centrality, hubs, clusters and another network metrics
+
+
+read_file = open("/Users/sandrofsousa/Google Drive/Mestrado USP/Dissertação/PTN Data/stop_times.txt", "r")
+
+print(read_file)
