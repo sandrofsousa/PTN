aefab7608caa6cac57ab358df826e23b9ceda5b6
sandrofsousa <sandrofsousa@gmail.com>
2015-09-21 14:58:03 -0300

    v1

---
+++ randon walk/weighted_network.py
@@ -0,0 +1,13 @@
+__author__ = 'sandrofsousa'
+
+# TODO Generate weight from travel time
+
+# TODO generate weight from link count
+
+# TODO generate weight from distance between nodes
+
+# TODO generate weight based on system capacity
+
+
+
+
