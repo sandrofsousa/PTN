90c59a2271d30eea894f57a45bb4f0dfae194da3
sandrofsousa <sandrofsousa@gmail.com>
2015-11-22 19:28:10 -0200

    Algorithm 1 finished and working. Code working for a fixed value of rho, define a vector for simulations.

--- gtfs_processing.py
+++ gtfs_processing.py
@@ -9,7 +9,7 @@ def get_stops_geodata():
     file = "/Users/sandrofsousa/Google Drive/Mestrado USP/Dissertação/PTN Data/GTFS/stops.txt"
 
     # temporary list to store data
-    result = []
+    geodata = []
     with open(file, "r", newline='') as data:
 
         # parse data using csv based on ',' position.
@@ -19,20 +19,20 @@ def get_stops_geodata():
         for line in searcher:
 
             # select the respective column of line based on ',' position.
-            stop_id = line[0]
-            stop_lat = line[3]
-            stop_lon = line[4]
+            stop_id = int(line[0])
+            stop_lat = float(line[3])
+            stop_lon = float(line[4])
+
             # append result to the list
-            result.append((stop_id, stop_lat, stop_lon))
+            geodata.append((stop_id, stop_lat, stop_lon))
 
         data.close()
-    return result
+    return geodata
 # get_stops_geodata()
 
 
 # Function to calculate distance in meters from two latitude and longitude.
 def calc_stops_distance(lat1, lon1, lat2, lon2):
-
     # approximate mean radius of earth in meters
     R = 6371000.0
 
@@ -47,26 +47,37 @@ def calc_stops_distance(lat1, lon1, lat2, lon2):
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
     distance = R * c
 
-    print(distance)
+    return distance
 # calc_stops_distance(lat1, lon1, lat2, lon2)
 
 
-lat1 = (-23.554190)
-lon1 = (-46.670723)
-lat2 = (-23.554190)
-lon2 = (-46.672000)
-
-
-def main():
+# Algorithm 1 to process GTFS file and return the list of stops in a proximity based on a rho vector.
+def algorithm_1(rho):
+    stops = []
     data = get_stops_geodata()
-    rho = 30
-    # loop
-    for line in data:
 
-        # select the first column from line 1 and 2 (position 0).
-        trip_old = line1.split(',')[0]
-        trip_new = line2.split(',')[0]
+    # loop reading the lists of stops, ignoring the last stop.
+    for row1 in range(len(data) - 1):
+        # Get values from first row.
+        stop1 = data[row1][0]
+        lat1 = data[row1][1]
+        lon1 = data[row1][2]
 
-        # select the fourth column from line 1 and 2 (position 3).
-        stop_old = line1.split(',')[3]
-        stop_new = line2.split(',')[3]
+        # loop reading the lists of stops, skipping the first row.
+        for row2 in range(row1 + 1, len(data)):
+            # Get values from second row.
+            stop2 = data[row2][0]
+            lat2 = data[row2][1]
+            lon2 = data[row2][2]
+
+            # call function to calculate the distance between two stops coordinates.
+            distance = calc_stops_distance(lat1, lon1, lat2, lon2)
+
+            # If distance <= rho, save two stops - they are close each other. Else, keep searchin on file.
+            if distance <= rho:
+                stops.append((stop1, stop2))
+            else:
+                continue
+    return stops
+
+
